f246174a0c03df1fe906945ada651a86fe5064a1
Robert Kein <robertkejn@gmail.com>
2023-02-21 19:54:41 +0400

    initial commit

---
+++ ht_3_1.1/ht_3_1.1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <Windows.h>
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Входные данные:  ";
+    std::string str;
+    std::getline(std::cin, str);
+    std::stringstream sstr(str);
+    std::vector<int> vec;
+    int number = 0;
+    while (sstr>>number)
+    {
+        vec.push_back(number);
+    }
+    std::for_each(vec.begin(), vec.end(), [](int& n) {
+        if (n % 2 == 1)
+            n = n * 3;
+        });
+    std::cout << "Выходные данные:  ";
+    std::for_each(vec.begin(), vec.end(), [](int& n) {std::cout << n << " "; });
+}
